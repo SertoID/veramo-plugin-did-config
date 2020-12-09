e258a37a1a3e086668cde3fca8def30e983f19b8
Mircea Nistor <mirceanis@gmail.com>
2020-12-09 19:32:27 +0100

    chore: remove unused plugin components

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,4 @@
 const schema = require('../plugin.schema.json')
 export { schema }
 export { DIDConfigurationPlugin } from './agent/did-configuration-plugin'
-export { KeyManagementSystem } from './key-manager/key-management-system'
-export { KeyStore } from './key-manager/key-store'
-export { SecretBox } from './key-manager/secret-box'
-export { IdentityProvider } from './identity-manager/identity-provider'
-export { IdentityStore } from './identity-manager/identity-store'
 export * from './types/IWellKnownDidConfigurationPlugin'
