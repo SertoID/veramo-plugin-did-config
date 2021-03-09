d3bcb22605e23aa04142d8fc20a075683bcb7018
Ese Mentie <ese.mentie@gmail.com>
2021-03-09 18:30:35 +0100

    chore: Troubleshooting errors with report not uploaded

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,7 @@ module.exports = {
     "!src/**/*.d.ts",
     "!**/node_modules/**"
   ],
+  "coverageReporters": ["text", "lcov", "json"],
   testEnvironment: "node",
   setupFilesAfterEnv: ["jest-extended"]
 }
